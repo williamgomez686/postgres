4cac9e3039235bcc7a5565b158998d5b9e3a524f
williamUbuntuLite <wgomez@motivacioncristiana.org>
2021-10-15 22:25:46 -0600

    Primer Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace postgres.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
