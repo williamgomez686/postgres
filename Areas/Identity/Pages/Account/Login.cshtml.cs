3ee9d280e4cd48e2705b2f07629b0aecc9738b82
williamUbuntuLite <wgomez@motivacioncristiana.org>
2021-10-16 21:55:52 -0600

    Antes de agregar todas las paginas con scaffoldig

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -43,15 +43,15 @@ namespace postgres.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            [Required]
+            [Required (ErrorMessage = "Debe ingresar su correo")]
             [EmailAddress]
             public string Email { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "la contraseña es nesesaria")]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Recordarme?")]
             public bool RememberMe { get; set; }
         }
 
